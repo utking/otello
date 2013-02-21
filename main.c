c967484bce0ab6e1ae667fb8c9c05ba8dced5695
utking <utking@mail.ru>
2013-02-21 21:09:09 +1100

    added sdl skeleton

--- main.c
+++ main.c
@@ -1,12 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "otello.h"
+#include <unistd.h>
 
 Field *board;
 
 int main(int argc, char *argv[])
 {
+	int running = 1;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL_Init failed\n");
+		return 1;
+	}
+	SDL_Surface *SurfDisplay = SDL_SetVideoMode(
+			640, 640, 32,
+			SDL_HWSURFACE | SDL_DOUBLEBUF);
+
+	SDL_WM_SetCaption("Otello", "Game");
+
+	SDL_Surface* boardSurface = loadBackground();
+	if (boardSurface)
+	{
+		SDL_Rect destRect;
+		destRect.x = 0;
+		destRect.y = 0;
+		destRect.w = 0;
+		destRect.h = 0;
+
+		SDL_BlitSurface(boardSurface, NULL, SurfDisplay, &destRect);
+		SDL_Flip(SurfDisplay);
+		SDL_Event event;
+
+		while (running)
+		{
+			while (SDL_PollEvent(&event))
+			{
+				running = onEvent(&event);
+			}
+			onLoop();
+			onRender();
+			usleep(1350);
+		}
+	}
+	else
+	{
+		return 1;
+	}
+
+	SDL_Quit();
+
 	printf("%s\n", "Otello. The Game");
 	board = createBoard();
 	if (!board)
